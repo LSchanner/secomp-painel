654fd6999994a24af322e0e97b53528e99871758
Henrique Noronha Facioli <henrique.facioli@students.ic.unicamp.br>
2015-05-05 00:21:43 -0300

    Divisão de routes para deixar mais claro

--- client/js/app.js
+++ client/js/app.js
@@ -1,75 +1,3 @@
-// Routes e coisas relativas à navegação/interação com o aplicativo
-
-//hook de obrigar o login
-loginHook = function() {
-    if(Meteor.user() === null){
-        this.redirect('login');
-    }
-    this.next();
-}
-
-// Página de login
-Router.route('/login', function () {
-    this.render('login');
-},{name:'login'});
-
-// Página de Cadastro
-Router.route('/cadastro', function () {
-    this.render('cadastro');
-},{name:'cadastro'});
-
-
-// Dashboard
-Router.route('/', function () {
-    this.layout('painel');
-    this.render('dashboard');
-},{ name:'dashboard',
-    onBeforeAction: loginHook });
-
-// Notícias
-Router.route('/noticias', function () {
-    this.layout('painel');
-    this.render('pagnoticias');
-},{ name:'noticias',
-    onBeforeAction: loginHook });
-
-// Atividades
-Router.route('/atividades', function () {
-    this.layout('painel');
-    this.render('atividades');
-},{ name:'atividades',
-    onBeforeAction: loginHook });
-
-// calendario
-Router.route('/calendario', function () {
-    this.layout('painel');
-    this.render('calendario');
-},{ name:'calendario',
-    onBeforeAction: loginHook });
-
-// achievements
-Router.route('/achievements', function () {
-    this.layout('painel');
-    this.render('achievements');
-},{ name:'achievements',
-    onBeforeAction: loginHook });
-
-// loja
-Router.route('/loja', function () {
-    this.layout('painel');
-    this.render('loja');
-},{ name:'loja',
-    onBeforeAction: loginHook });
-
-// Mapa
-Router.route('/mapa', function () {
-    this.layout('painel');
-    this.render('mapa');
-},{ name:'mapa',
-    onBeforeAction: loginHook });
-
-
-
 Template.painel.helpers({
     atRoute: function(path){
         return path === Router.current().route.getName();
@@ -87,11 +15,10 @@ Template.painel.events({
     'click .navbar-collapse ul li a': function(){
         $('.navbar-collapse').collapse('hide');
     },
-    
+
     'click #logout-button':function(){
         Meteor.logout(function() {
             Router.go('/');
-            console.log('Bye Meteorite! Come back whenever you want!');
         });
     }
 });
---
+++ client/js/routes.js
@@ -0,0 +1,68 @@
+/* Routes e coisas relativas à navegação/interação com o aplicativo */
+
+// Hook de obrigar o login a ser feito na página '/'
+loginHook = function() {
+    if(Meteor.user() === null){
+        this.redirect('login');
+    }
+    this.next();
+}
+
+// Página de login
+Router.route('/login', function () {
+    this.render('login');
+},{name:'login'});
+
+// Página de Cadastro
+Router.route('/cadastro', function () {
+    this.render('cadastro');
+},{name:'cadastro'});
+
+// Dashboard
+Router.route('/', function () {
+    this.layout('painel');
+    this.render('dashboard');
+},{ name:'dashboard',
+    onBeforeAction: loginHook });
+
+// Notícias
+Router.route('/noticias', function () {
+    this.layout('painel');
+    this.render('pagnoticias');
+},{ name:'noticias',
+    onBeforeAction: loginHook });
+
+// Atividades
+Router.route('/atividades', function () {
+    this.layout('painel');
+    this.render('atividades');
+},{ name:'atividades',
+    onBeforeAction: loginHook });
+
+// calendario
+Router.route('/calendario', function () {
+    this.layout('painel');
+    this.render('calendario');
+},{ name:'calendario',
+    onBeforeAction: loginHook });
+
+// achievements
+Router.route('/achievements', function () {
+    this.layout('painel');
+    this.render('achievements');
+},{ name:'achievements',
+    onBeforeAction: loginHook });
+
+// loja
+Router.route('/loja', function () {
+    this.layout('painel');
+    this.render('loja');
+},{ name:'loja',
+    onBeforeAction: loginHook });
+
+// Mapa
+Router.route('/mapa', function () {
+    this.layout('painel');
+    this.render('mapa');
+},{ name:'mapa',
+    onBeforeAction: loginHook });
