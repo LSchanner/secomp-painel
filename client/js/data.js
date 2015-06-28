66828e5d8d8a6bf08cc4899c663d56923476d2ed
Henrique Noronha Facioli <henrique.facioli@students.ic.unicamp.br>
2015-06-28 16:55:01 -0300

    adição achievements

--- client/js/data.js
+++ client/js/data.js
@@ -1,6 +1,7 @@
 Meteor.subscribe('Noticias');
 Meteor.subscribe('Atividades');
 Meteor.subscribe("Credenciamentos");
+Meteor.subscribe('Achievements');
 Meteor.subscribe("allUserData");
 Meteor.subscribe("UserData");
 moment.lang('pt');
